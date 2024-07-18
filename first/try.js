33f44aad8d717f770a36a08cf63ee48d8f355331
Adarsh Tripathi <128805119+sebastian-sultz@users.noreply.github.com>
2024-07-18 14:05:11 +0000

    functions notes

--- first/try.js
+++ first/try.js
@@ -1,1 +1,36 @@
-console.log("Adarsh");
+// console.log("Adarsh");
+
+
+func(10)
+function func(a){
+    console.log(a)
+}
+
+// and
+
+
+function func(a){
+    console.log(a)
+}
+func(10)
+
+// and new way of initializing the function
+const newFunc = function(b)
+{
+    console.log(b);
+}
+newFunc(12)
+
+
+// but
+
+// newFunc(12)   this will create an error (concept of hoisting)
+// const newFunc = function(b)
+// {
+//     console.log(b);
+// }
+
+
+
+
+
